3fcf5d04c4a6c6a973bf4fcf3d5b5a0afbfff01f
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2021-02-03 18:32:36 -0800

    datatypes in groovy

---
+++ GroovyFundamentals/src/variablesingroovy/DataTypes.groovy
@@ -0,0 +1,67 @@
+package variablesingroovy
+// data types in groovy
+
+byte a = 12
+
+println a
+println a.MIN_VALUE
+println a.MAX_VALUE
+
+println '********************************'
+
+short b = 12
+
+println b
+println b.MIN_VALUE
+println b.MAX_VALUE
+
+println '********************************'
+
+int c = 12
+
+println c
+println c.MIN_VALUE
+println c.MAX_VALUE
+
+println '********************************'
+
+long d = 12
+
+println d
+println d.MIN_VALUE
+println d.MAX_VALUE
+
+println '********************************'
+
+float e = 12.0f
+
+println e
+println e.MIN_VALUE
+println e.MAX_VALUE
+
+println '********************************'
+
+double f = 12.0f
+
+println f
+println f.MIN_VALUE
+println f.MAX_VALUE
+
+println '********************************'
+
+char ab = 'a'
+
+println ab
+
+println '********************************'
+
+String str = ' harshil'
+
+println str
+
+println '********************************'
+
+boolean flag = true
+println flag
+
+println '********************************'
