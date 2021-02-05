27fa8a3f2b627aac11af93b938e18dbcead37dc3
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2021-02-04 19:18:51 -0800

    operators

---
+++ GroovyFundamentals/src/variablesingroovy/Operators.groovy
@@ -0,0 +1,11 @@
+package variablesingroovy;
+
+//operators in Groovy
+// assert is used to check if the statement is true or false 
+assert 1+2==3
+assert 2-1==1
+assert 3*4==12
+assert 4/4==1
+assert 10%3==1
+assert 2 ** 2==4// power **
+assert 9.intdiv(5)==1
