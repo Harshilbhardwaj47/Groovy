84903f3c5b14f2fccc108204be0a200680d57a11
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2021-04-23 15:27:29 -0700

    jenkins file exception handeling

--- GroovyFundamentals/src/exceptionhandling/Exception.groovy
+++ GroovyFundamentals/src/exceptionhandling/Exception.groovy
@@ -2,13 +2,45 @@ package exceptionhandling
 
 class Example {
 	static void main(String[] args) {
-	   try {
-		  def arr = new int[3];
-		  arr[5] = 5;
-	   } catch(Exception ex) {
-		  println("Catching the exception");
-	   }
-		 
-	   println("Let's move on after the exception");
+		try {
+			node('master'){
+
+				stage('Code Checkout')
+				cleanWs()
+
+				status.check([
+
+					branch: 'main',
+
+					repo: 'https://github.com/Harshilbhardwaj47/spring-petclinic.git'
+
+				])
+
+				def specs = [:]
+				def specsDir = "./Specs/V1.0"
+
+				if(fileExists(specsDir + "/ci-spec.yml")){
+					ci_specs = readYaml file : specsDir + "/ci-spec.yml"
+					specs = specs + ci_specs
+				}
+
+				stage('Code build')
+
+
+
+				build.check([
+
+
+					command1 : specs.Build.command1,
+
+
+
+
+				])
+			}
+		} catch(Exception ex) {
+			println ex.getCause();
+			println ex.getMessage();
+		}
 	}
- }// use class while doing exception handling it helps out 
+}// use class while doing exception handling it helps out
